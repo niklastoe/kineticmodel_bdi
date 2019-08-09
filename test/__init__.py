67a289c917183059bfcc3605371c76ce95edcfcc
Unknown <niklas.toetsch@stud.uni-due.de>
2019-08-09 12:22:30 +0200

    add reproduction test for 2nd order reaction

--- test/__init__.py
+++ test/__init__.py
@@ -5,10 +5,11 @@ import pandas as pd
 from workflows.kinetic_modeling import KineticModel
 
 
-class TestKineticModel(ut.TestCase):
+class TestKineticModelBase(ut.TestCase):
+    __test__ = False
 
     def __init__(self, *args, **kwargs):
-        super(TestKineticModel, self).__init__(*args, **kwargs)
+        super(TestKineticModelBase, self).__init__(*args, **kwargs)
 
         self.time = np.arange(0.,3600., 300.)
         self.k = -2.0
@@ -18,12 +19,10 @@ class TestKineticModel(ut.TestCase):
         self.model = KineticModel(self.true_data, self.reactions, self.parameters, educts='A')
 
     def generate_reactions(self):
-        return [[{'A': 1}, {'P': 1}, 'k']]
+        raise NotImplementedError
 
-    def integrated_rate_law(self, A0, t, k):
-        k_applied = 10**k
-        At = A0 * np.exp(-k_applied*t)
-        return At
+    def integrated_rate_law(self):
+        raise NotImplementedError
 
     def generate_true_data(self):
         """generate a DataFrame containing kinetic data from integrated rate law"""
@@ -42,8 +41,32 @@ class TestKineticModel(ut.TestCase):
     def test_reproduction_integrated_rate_law(self):
         """kinetic model should yield identical result to integrated rate law"""
         diff = self.true_data - self.model.modeled_data
-        self.assertAlmostEqual(diff.sum().sum(),0., delta=1e-11)
-        self.assertLess(diff.max().max(), 1e-12)
+        self.assertAlmostEqual(diff.sum().sum(), 0., delta=1e-9)
+        self.assertLess(diff.max().max(), 1e-9)
+
+
+class TestKineticModelFirst(TestKineticModelBase):
+    __test__ = True
+
+    def generate_reactions(self):
+        return [[{'A': 1}, {'P': 1}, 'k']]
+
+    def integrated_rate_law(self, A0, t, k):
+        k_applied = 10**k
+        At = A0 * np.exp(-k_applied*t)
+        return At
+
+
+class TestKineticModelSecond(TestKineticModelBase):
+    __test__ = True
+
+    def generate_reactions(self):
+        return [[{'A': 2}, {'P': 1}, 'k']]
+
+    def integrated_rate_law(self, A0, t, k):
+        k_applied = 10**k
+        At = 1. / (k_applied*t + 1./A0)
+        return At
 
 
 if __name__ == '__main__':
