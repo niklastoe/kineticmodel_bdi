b0cfdfafcc4aa7de0f2a4cc796eb045b1ffdcd8c
Unknown <niklas.toetsch@stud.uni-due.de>
2019-09-05 11:15:13 +0200

    check if h5 file is defined before trying to write
    otherwise, it won't work if not writing to disk

--- sampling.py
+++ sampling.py
@@ -202,11 +202,12 @@ def sample_until_convergence(sampler, nsteps, starting_pos, thin_by=1):
         autocorr.append(np.mean(tau))
         index += 1
 
-        # store in h5 file
+        # store in h5 file if one is written
         tau_df = pd.DataFrame(tau.reshape(1,-1),
                               columns=sampler.parm_names,
                               index=[sampler.iteration])
-        tau_df.to_hdf(sampler.backend.filename, 'autocorrelation', format='table', append=True)
+        if hasattr(sampler.backend, 'filename'):
+            tau_df.to_hdf(sampler.backend.filename, 'autocorrelation', format='table', append=True)
 
         # Check convergence
         converged = np.all(tau * min_iid < sampler.iteration)
