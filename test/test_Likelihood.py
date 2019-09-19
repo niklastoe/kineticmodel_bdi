f46dc8e3a966bc07f954e57419f38fa9693a67eb
Niklas <niklas.toetsch@stud.uni-due.de>
2019-09-19 13:51:18 +0200

    add tests for Likelihood if a function, not KineticModel is used

--- test/test_Likelihood.py
+++ test/test_Likelihood.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 import unittest as ut
 import xarray as xr
 
@@ -5,15 +7,8 @@ from workflows.kinetic_modeling.test import test_KineticModel
 from workflows.kinetic_modeling.bayesian_framework import Likelihood, OrdinaryStandardDeviation
 
 
-class TestLikelihood(test_KineticModel.TestKineticModelFirst):
-    __test__ = True
-
-    def __init__(self, *args, **kwargs):
-        super(TestLikelihood, self).__init__(*args, **kwargs)
-
-        self.model.create_native_odesys()
-        self.sel_std_dev = 1e-7
-        self.likelihood_ordinary_obj = Likelihood(self.model, OrdinaryStandardDeviation(self.sel_std_dev))
+class TestLikelihoodBase(test_KineticModel.TestKineticModelFirst):
+    __test__ = False
 
     def test_likelihood_creation(self):
         self.assertTrue(isinstance(self.likelihood_ordinary_obj, Likelihood))
@@ -33,10 +28,51 @@ class TestLikelihood(test_KineticModel.TestKineticModelFirst):
 
         samples = xr.concat([df.to_xarray() for df in samples], "samples")
 
-        mu = samples.mean(dim='samples').to_dataframe() - self.model.exp_data
+        y_exp = self.likelihood_ordinary_obj.evaluate_parameters(self.get_parameters(), return_exp_data=True)
+
+        if type(samples) == xr.core.dataarray.DataArray:
+            samples = samples.to_dataset(name='placeholder')
+            y_exp = pd.DataFrame(y_exp, columns=['placeholder'])
+
+        y_model = samples.mean(dim='samples').to_dataframe()
+        mu = y_model - y_exp
         std = samples.std(dim='samples').to_dataframe()
-        self.assertAlmostEqual(mu.mean().mean(), 0, delta=self.model.exp_data.max().max() * 5e-3)
+
+        self.assertAlmostEqual(mu.mean().mean(), 0, delta=self.likelihood_ordinary_obj.exp_data_formatted.max().max() * 5e-3)
         self.assertAlmostEqual(std.mean().mean(), self.sel_std_dev, delta=self.sel_std_dev*0.1)
+
+
+class TestLikelihoodKineticModel(TestLikelihoodBase):
+    __test__ = True
+
+    def __init__(self, *args, **kwargs):
+        super(TestLikelihoodKineticModel, self).__init__(*args, **kwargs)
+
+        self.model.create_native_odesys()
+        self.sel_std_dev = 1e-7
+        self.likelihood_ordinary_obj = Likelihood(self.model, OrdinaryStandardDeviation(self.sel_std_dev))
+
+
+class TestLikelihoodFunction(TestLikelihoodBase):
+    __test__ = True
+
+    def __init__(self, *args, **kwargs):
+        super(TestLikelihoodFunction, self).__init__(*args, **kwargs)
+
+        self.range = np.arange(0, 10, 0.5)
+
+        def line(parameters):
+            return self.range * parameters['m'] + parameters['b']
+
+        def get_line_parameters():
+            return {'m': 1., 'b': 5.}
+
+        self.get_parameters = get_line_parameters
+        exp_data_formatted = pd.Series(line(self.get_parameters()), index=self.range)
+        self.sel_std_dev = 1e-7
+        self.likelihood_ordinary_obj = Likelihood(line,
+                                                  exp_data_formatted=exp_data_formatted,
+                                                  std_deviation_obj=OrdinaryStandardDeviation(self.sel_std_dev))
 
 if __name__ == '__main__':
     ut.main(verbosity=2)
