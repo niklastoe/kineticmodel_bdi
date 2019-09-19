e3b1c91683303f7a8338ca6d5b4a46a043f7a5ee
Niklas <niklas.toetsch@stud.uni-due.de>
2019-09-19 13:52:04 +0200

    add comments for likelihood tests

--- test/test_Likelihood.py
+++ test/test_Likelihood.py
@@ -19,8 +19,10 @@ class TestLikelihoodBase(test_KineticModel.TestKineticModelFirst):
         self.assertAlmostEqual(likelihood, self.likelihood_ordinary_obj.max_likelihood, delta=1e-8)
 
     def test_evaluation(self):
-        """Sampling should allow to recover input std_dev and mu (=y_model=y_exp in this case)"""
+        """Posterior prediction should allow to recover input std_dev and mu (=y_model=y_exp in this case) by using the
+        correct input parameters"""
 
+        # get 100 samples for the true parameters
         samples = []
 
         for x in range(100):
@@ -28,17 +30,23 @@ class TestLikelihoodBase(test_KineticModel.TestKineticModelFirst):
 
         samples = xr.concat([df.to_xarray() for df in samples], "samples")
 
+        # get the true observed values
         y_exp = self.likelihood_ordinary_obj.evaluate_parameters(self.get_parameters(), return_exp_data=True)
 
+        # if observed/experimental data are stored in pd.Series, need to make sure that everything ends as a DataFrame
+        # otherwise, comparison will be messed up
         if type(samples) == xr.core.dataarray.DataArray:
             samples = samples.to_dataset(name='placeholder')
             y_exp = pd.DataFrame(y_exp, columns=['placeholder'])
 
+        # compare observed data to modeled data (from parameters) and check the standard deviation
         y_model = samples.mean(dim='samples').to_dataframe()
         mu = y_model - y_exp
         std = samples.std(dim='samples').to_dataframe()
 
+        # mu should be close to 0 (input parameters used for posterior prediction)
         self.assertAlmostEqual(mu.mean().mean(), 0, delta=self.likelihood_ordinary_obj.exp_data_formatted.max().max() * 5e-3)
+        # sampled standard deviation should not differ by more than 10% from real standard deviation
         self.assertAlmostEqual(std.mean().mean(), self.sel_std_dev, delta=self.sel_std_dev*0.1)
 
 
