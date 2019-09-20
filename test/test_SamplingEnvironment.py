a00a04c401c141e1f18a24e74582ccc86a2b42a5
Niklas <niklas.toetsch@stud.uni-due.de>
2019-09-20 12:29:21 +0200

    test reformatting parameters

--- test/test_SamplingEnvironment.py
+++ test/test_SamplingEnvironment.py
@@ -1,3 +1,4 @@
+import copy
 import unittest as ut
 from workflows.kinetic_modeling.test import test_Likelihood
 from workflows.kinetic_modeling.sampling import SamplingEnvironment, UniformMinMax
@@ -18,11 +19,20 @@ class TestSamplingEnvironment(test_Likelihood.TestLikelihoodFunction):
 
         self.env = SamplingEnvironment(prior_distributions, likelihood_object_dict)
 
+        def reformat_parameters(parameters):
+            formatted_parameters = copy.deepcopy(parameters)
+            formatted_parameters['b'] = parameters['c'] + 1
+            return formatted_parameters
+
+        self.env_reformatted = SamplingEnvironment(prior_distributions, likelihood_object_dict, reformat_parameters)
+
     def test_required_parameters(self):
         """are all required parameters detected correctly?"""
         theta = set(['m', 'b'])
+        theta_reformatted = set(['m', 'c'])
 
         self.assertEqual(theta, set(self.env.required_parameters))
+        self.assertEqual(theta_reformatted, set(self.env_reformatted.required_parameters))
 
 if __name__ == '__main__':
     ut.main(verbosity=2)
