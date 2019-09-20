1f3f1096a31a3d4c53dde99d815ca828664f028f
Niklas <niklas.toetsch@stud.uni-due.de>
2019-09-20 14:55:50 +0200

    calculate K_Eq between two species given a dictionary of parameters
    this dictionary must of course contain the conversion rate constants

--- __init__.py
+++ __init__.py
@@ -327,6 +327,20 @@ class KineticModel(object):
         else:
             self.modeled_data = modeled_data
 
+    def determine_equilibrium_constant(self, A, B, new_parameters):
+        """calculate the equilibrium constant between species A and B given a dictionary of parameters containing
+        conversion rate constants"""
+        for x in self.reaction_list_input:
+            if (x[0].keys() == [A]) and (x[1].keys() == [B]):
+                k_AB = x[-1]
+            elif (x[0].keys() == [B]) and (x[1].keys() == [A]):
+                k_BA = x[-1]
+        if new_parameters:
+            K_Eq = new_parameters[k_AB] - new_parameters[k_BA]
+        else:
+            K_Eq = self.parameters[k_AB] - self.parameters[k_BA]
+        return K_Eq
+
     def get_observed_activity(self, concentrations, starting_conc, observable):
         """return activity of desired observable from all concentrations"""
         if observable == 'educt':
